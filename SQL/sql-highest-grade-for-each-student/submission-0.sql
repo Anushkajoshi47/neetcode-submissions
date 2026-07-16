27cb306efe97c13a689761e346f6cb2aa5115011
Anushka Chintan Joshi <anushkajoshi501@gmail.com>
2026-07-16 19:55:59 +0530

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT student_id, exam_id, score
+FROM(
+    SELECT student_id, exam_id, score,
+    ROW_NUMBER() OVER(PARTITION by student_id ORDER BY score DESC, exam_id ASC) as rn
+    FROM exam_results
+) ranked_exams
+WHERE rn = 1
+ORDER BY student_id ASC;
